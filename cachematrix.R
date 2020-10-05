72115f8d4e9f92ff810c6e1ae1871aa5422a4a0f
Haoran Chen <chenhaoran.earth@live.com>
2020-10-05 16:54:00 +0800

    add inverse for matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,23 @@
 ## functions do
 
 ## Write a short comment describing this function
+## set: store the new value of matrix and clear the old inverse value
+## get: return the inverse result of matrix
+## set_inverse: store the inverse of matrix
+## get_inverse: return the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    set_inverse <- function(inverse_val) inverse <<- inverse_val
+    get_inverse <- function() inverse
+    
+    list(set = set, get = get, 
+         set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
@@ -12,4 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    cache <- x$get()
+    if(!is.null(cache)) {
+        message("Return cached inverse.")
+        cache
+    }
+    inverse <- solve(x$get(), ...)
+    x$set_inverse(inverse)
+    inverse
 }
